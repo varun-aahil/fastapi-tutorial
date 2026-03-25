9754acab62d5d2d24426f49e0939effe7a3864c8
Varun Aahil <haseena.anantapur@gmail.com>
2026-03-25 09:31:49 +0530

    Creating a \Loan Management System

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+def getData():
+    with open('test.json', 'r') as f:
+        return json.load(f)
 
 @app.get('/')
 def hello():
-    return {'message' : "Hello World"}
+    return {'message' : 'This is a Loan Managment System API'}
 
 @app.get('/about')
 def aboutInfo():
-    return {'message' : 'Hello this is a test demo from varun'}
+    return {'message' : 'This is an API to manage your customers loans'}
+
+@app.get('/customers')
+def getUsers():
+    data = getData()
+    return data
